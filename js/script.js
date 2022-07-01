1e1a71ca5b16b819d6cb6247fdb1430400042fd7
Anarasty <murloc.ninja16@gmail.com>
2022-07-01 17:17:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let number = 2;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+const obj = {
+    a:50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+
+var name = 'IVAN';
+console.log(name);
+
+{
+    var result = 55;
+}
+console.log(result);
+
