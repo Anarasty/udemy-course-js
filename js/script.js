ab98325ed7405f32b0151c1641212818ae64d150
Anarasty <murloc.ninja16@gmail.com>
2022-07-03 17:53:43 +0300

    1

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ const obj = {
 };
 
 obj.a = 10;
-
 console.log(obj);
 
 
@@ -22,3 +21,39 @@ console.log(name);
 }
 console.log(result);
 
+const vehicleWidth = 300;
+const vehicleLength = 500;
+console.log('Car width is: ' + vehicleWidth + ', car length: ' + vehicleLength);
+//snake_case
+//UPPER_CASE
+//Kebab-case
+//PascalCase
+
+let number1 = 4.3;
+let und;
+console.log(und);
+
+const object1 = {
+    name: 'Mark',
+    age: 23,
+    isProgger: true
+};
+console.log(object1.name);
+console.log(object1['name']);
+
+let array1 = ['superman.png', 'batman.png', 'greenlantern.png'];
+console.log(array1[0]);
+
+// ARRAYS AND OBJECTS
+const array2 = [1, 2, 3];
+console.log(array2[1]);
+array2[5] = 'hello';
+console.log(array2);
+// const object2 = {a:1, b:2, c:3};
+const object2 = {
+    'Anna': 500,
+    'Alice': 800
+};
+object2['Alan'] = 220;
+console.log(object2['Alan']);
+console.log(object2);
