3d6c1c7d9898080435ae659aafa15bb73603ad95
Anarasty <murloc.ninja16@gmail.com>
2022-07-09 18:31:06 +0300

    Loops++

--- js/script.js
+++ js/script.js
@@ -138,3 +138,58 @@ switch (num) {
         console.log('Not this time');
         break;
 }
+
+// LOOPS ++
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log('I am not hungry!');
+// }
+
+// console.log((hamburger && fries));
+
+const hamburger = 3;
+const fries = 1;
+const cola = 0;
+
+if (hamburger === 3 && cola === 1 && fries === 1) {
+    console.log('I am not hungry!');
+} else {
+    console.log('Still hungry!')
+}
+
+console.log(hamburger === 3 && cola && fries);
+
+console.log(1 && 0);
+console.log(1 && 5);
+console.log(null && 5);
+console.log(0 && 'sdasdsad');
+
+const hamburger1 = 3;
+const fries1 = 3;
+const cola1 = 0;
+const nuggets1 = 2;
+
+if (hamburger1 === 3 &&  cola1 === 2 || fries1 === 3 && nuggets1) {
+    console.log("It's OK");
+} else {
+    console.log("NOT ok");
+}
+console.log((hamburger1 || cola1 || fries1));
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log(!false); //!0
+
+console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null && !3 && undefined || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
+
