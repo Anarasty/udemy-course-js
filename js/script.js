75a9211fec8cdbeec725e868358e91475d155caa
Anarasty <murloc.ninja16@gmail.com>
2022-07-10 19:34:19 +0300

    Loops and loops ++

--- js/script.js
+++ js/script.js
@@ -107,7 +107,7 @@ console.log(isChecked && isClose);
 console.log(isChecked || isClose);
 console.log(isChecked || !isClose);
 
-// LOOPS
+//CONDITIONAL STATEMENTS
 if (2) { //0=false
     console.log('Good');
 } else {
@@ -139,7 +139,7 @@ switch (num) {
         break;
 }
 
-// LOOPS ++
+// CONDITIONAL STATEMENTS ++
 // const hamburger = 5;
 // const fries = null;
 
@@ -193,3 +193,82 @@ console.log( NaN || null || !3 || undefined || 5);
 console.log( NaN || null && !3 && undefined || 5);
 console.log( 5 === 5 && 3 > 1 || 5);
 
+//LOOPS
+let num1 = 50;
+
+while (num1 <= 55) {
+    console.log(num1);
+    num1++;
+}
+
+do {
+    console.log(num1);
+    num1++;
+} 
+while (num1 < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
+
+//LOOPS ++
+for (let i = 0; i < 3; i++) {
+    console.log(i + '--');
+    for (let j = 0; j < 3; j++){
+        console.log(j + '==');
+    }
+}
+
+//task make star pyramid
+//*
+//** 
+//*** 
+//**** 
+//***** 
+//******
+let result2 = '';
+const lengthStar = 7;
+for (let i = 1; i < lengthStar; i++) {
+    for (let j = 0; j < i; j++){
+        result2 += '*';
+    }
+    result2 += '\n';
+}
+
+console.log(result2);
+
+
+first: for (let i = 0; i < 3; i++) { //first is a mark
+    console.log(`First level: ${i}`);
+    for(let j = 0; j < 3; j++){
+        console.log(`Second level: ${j}`);
+        for(let k = 0; k < 3; k++){
+            if (k === 2) break first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let i = 2;
+
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
