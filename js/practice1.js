4dd48e1196a3ea1bfc2adab9474fda4a490e2d25
Anarasty <murloc.ninja16@gmail.com>
2022-07-11 18:42:10 +0300

    practice 3

--- js/practice1.js
+++ js/practice1.js
@@ -1,4 +1,13 @@
-const numberOfFilms = prompt('How much films have you watched?','');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = prompt('How much films have you watched?','');
+
+    while (numberOfFilms == '' || numberOfFilms == null ||isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('How much films have you watched?','');
+    }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -16,27 +25,47 @@ const personalMovieDB = {
 // personalMovieDB.movies[a] = b;
 // personalMovieDB.movies[c] = d;
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Last films that you watched...',''),
-          b = prompt('Rate film...','');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Last films that you watched...',''),
+              b = prompt('Rate film...','');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
+rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log('Films watched lower than 10. Try 10 < films < 30');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Films count ok')
-} else if (personalMovieDB.count >= 30) {
-    console.log('Super movie watcher');
-} else {
-    console.log('not enough films!!!error')
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Films watched lower than 10. Try 10 < films < 30');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Films count ok')
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Super movie watcher');
+    } else {
+        console.log('not enough films!!!error');
+    }
 }
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Your favorite jenre is (chose number) - ${i}`);
+    }
+}
+writeYourGenres();
 
 console.log(personalMovieDB);
