04bf6a0f28b0119cefc3237441a37f31694d4a5f
Anarasty <murloc.ninja16@gmail.com>
2022-07-24 21:07:44 +0300

    iterable constructs

--- advanced js basics/script1.js
+++ advanced js basics/script1.js
@@ -135,9 +135,77 @@ Object.defineProperties(user, {
     surname: {writable: false}
 });
 
-
 // Object.defineProperty(user, 'gender', {value:'male'});
 // console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
 // writable
 // enumerable
 // configurable
+
+
+//ITERABLE CONSTRUCTS
+
+const user1 = {
+    name: 'Ann',
+    surname: 'Johnson',
+    birthday: '13/05/2000',
+    showMyPublicData: function () {
+        console.log(`${this.name} ${this.surname}`);
+    }
+};
+
+for (const key in user1) {
+    console.log(user1[key]);
+}
+
+const arr = ['b', 'a', 'c'];
+Array.prototype.someMethod = function () {};
+
+// console.dir(arr);
+
+for (const key in arr) {
+    console.log(arr[key]);
+}
+
+for (const key of arr) {
+    console.log(key);
+}
+
+const str = 'string';
+
+for (const key in str) {
+    console.log(str[key]);
+}
+
+
+
+const salaries = {
+    john: 500,
+    ivan: 1000,
+    ann: 5000,
+    sayHello: function () {
+        console.log("Hello!");
+    }
+};
+
+salaries[Symbol.iterator] = function () {
+    return {
+        current: this.john,
+        last: this.ann,
+        next() {
+            if (this.current < this.last) {
+                this.current = this.current + 500;
+                return {done: false, value: this.current};
+            } else {
+                return {done: true};
+            }
+            // {done: true, value: 123}
+        }
+    };
+};
+
+const iterator = salaries[Symbol.iterator]();
+console.log(iterator.next());
+
+// for (let res of salaries) {
+//     console.log(res);
+// }
