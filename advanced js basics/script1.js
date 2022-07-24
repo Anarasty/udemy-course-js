6e719d4a90d3fd61b102bb59c3e8160b47248b5b
Anarasty <murloc.ninja16@gmail.com>
2022-07-24 19:07:41 +0300

    property descriptors

--- advanced js basics/script1.js
+++ advanced js basics/script1.js
@@ -109,3 +109,35 @@ myAwesomeDB.id = '323232';
 
 console.log(myAwesomeDB[Symbol.for('id')]);
 console.log(myAwesomeDB);
+
+
+//PROPERTY DESCRIPTORS AND OBJECT METHODS
+const birthday = Symbol('birthday');
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    [birthday]: '20/04/1993',
+    showMyPublicData: function () {
+        console.log(`${this.name} ${this.surname}`);
+    }
+};
+// Object.defineProperty(user, 'birthday', {value: prompt('Date?'), enumerable: true, configurable: true});
+
+// console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));
+
+Object.defineProperty(user, 'showMyPublicData', {enumerable: false});
+
+for (let key in user) console.log(key);
+console.log(Object.getOwnPropertyDescriptor(user, birthday));
+
+Object.defineProperties(user, {
+    name: {writable: false},
+    surname: {writable: false}
+});
+
+
+// Object.defineProperty(user, 'gender', {value:'male'});
+// console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
+// writable
+// enumerable
+// configurable
