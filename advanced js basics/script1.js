680ef0d93466148b28ed22aad81dd766be4750ba
Anarasty <murloc.ninja16@gmail.com>
2022-07-24 10:24:09 +0300

    Optional chaining

--- advanced js basics/script1.js
+++ advanced js basics/script1.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//NULLISH COALESCING
 const box = document.querySelector('.box');
 
 const newHeight = 100;
@@ -16,3 +17,31 @@ changeParams(box, newHeight, newWidth);
 let userName;
 let userKey;
 console.log(userKey ?? userName ?? 'User'); //nullish coalescing
+
+//OPTIONAL CHAINING
+const block = document.querySelector('.block');
+console.log(block);
+
+// if (block) {
+//     console.log(block.textContent);
+// }
+
+console.log(block?.textContent); //optional chaining
+
+console.log(1 + 2);
+
+const userData = {
+    name: 'Ivan',
+    age: null,
+    say: function () {
+        console.log('Hello');
+    }
+};
+userData.say();
+userData.hey?.();
+
+// if (userData && userData.skills && userData.skills.js) {
+//     console.log(userData.skills.js);
+// }
+
+console.log(userData.skills?.js);
