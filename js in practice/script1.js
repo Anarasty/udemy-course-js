c38581697096401cee3303fece5eb875dc0896ff
Anarasty <murloc.ninja16@gmail.com>
2022-07-29 19:33:52 +0300

    working with dates

--- js in practice/script1.js
+++ js in practice/script1.js
@@ -41,3 +41,31 @@ wrapper.addEventListener('click', (event) => { //EVENT DELEGATION
 const btn = document.createElement('button');
 btn.classList.add('red');
 wrapper.append(btn);
+
+
+
+//WORKING WITH DATES
+// const now = new Date(2020, 5, 1, 20);
+const now = new Date('2020-05-01');
+// new Date.parse('2020-05-01');
+
+console.log(now.setHours(60));
+console.log(now);
+
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getUTCHours());
+
+console.log(now.getTimezoneOffset());
+console.log(now.getTime());
+
+let start = new Date();
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+alert(`Цикл отработал за ${end-start} милисекунд`);
