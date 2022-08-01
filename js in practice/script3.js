0fc178357610151479c16c5e0ba91cc2a6e957a6
Anarasty <murloc.ninja16@gmail.com>
2022-08-01 17:58:05 +0300

    ES6 classes

--- js in practice/script3.js
+++ js in practice/script3.js
@@ -123,3 +123,38 @@ obj1.sayNumber();
 
 const doubles = a => a * 2;
 console.log(doubles(4));
+
+
+//ES6 CLASSES
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+
+    }
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor){
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+div.showMyProps();
+console.log(div.calcArea());
+
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(square.calcArea());
+console.log(long.calcArea());
