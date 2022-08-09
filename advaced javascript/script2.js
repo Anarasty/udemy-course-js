ad213544d7b51c81f27efc0328f753d8616c66d9
Anarasty <murloc.ninja16@gmail.com>
2022-08-09 15:01:40 +0300

    incapsulation

--- advaced javascript/script2.js
+++ advaced javascript/script2.js
@@ -83,3 +83,33 @@ const persone1 = {
 
 console.log(persone1.userAge = 30);
 console.log(persone1.userAge);
+
+/// INCAPSULATION
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+    
+    say () {
+        console.log(`Users name is - ${this.name}, and age is - ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Syntax err');
+        }
+    }
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan._age);
+ivan._age = 99;
+console.log(ivan._age);
+ivan.say();
